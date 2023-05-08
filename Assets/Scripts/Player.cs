6b8850e36a2d578a653fff4305fdf1dc79f508e3
azeTrom <nmort17@aol.com>
2023-05-08 13:35:58 -0500

    finished tethers, reel, and gasboost

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,15 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     //to do:
-    //Crosshairs
-    //tether swing
-    //reel
-    //gas
     //gas meter
     //missile
     //turret
@@ -30,20 +27,42 @@ public class Player : MonoBehaviour
     public Rigidbody rb;
     public Transform anchors;
 
+    public TMP_Text leftCrosshair;
+    public TMP_Text rightCrosshair;
+
     public LineRenderer leftLineRenderer;
     public LineRenderer rightLineRenderer;
 
-    public Transform leftAnchor;
-    public Transform rightAnchor;
+    public GameObject leftAnchor;
+    public GameObject rightAnchor;
 
-    public ConfigurableJoint leftJoint;
-    public ConfigurableJoint rightJoint;
+    private SpringJoint leftJoint;
+    private SpringJoint rightJoint;
 
-    //rotate player with mouse:
+    //custom gravity
+    private readonly float gravityScale = 3;
+
+    //custom drag
+    private float drag; //set in Gas()
+
+    //rotate player with mouse
     private readonly float rotateSpeed = 8;
 
     //fires raycast through crosshairs
     private readonly float raycastOffset = .23f;
+
+    //tether
+    private readonly int maxTetherRange = 700;
+
+    //reel
+    private bool leftReeling;
+    private bool rightReeling;
+    private readonly float reelAmount = 100;
+    //increase when reeling both tethers
+    private readonly float doubleReelAmount = 200;
+
+    //gas
+    private readonly float gasBoost = 2;
 
     private void Start()
     {
@@ -55,11 +74,21 @@ public class Player : MonoBehaviour
         Cursor.visible = false;
     }
 
+    private void FixedUpdate()
+    {
+        //custom gravity
+        Vector3 gravity = -9.81f * gravityScale * Vector3.up;
+        rb.AddForce(gravity, ForceMode.Acceleration);
+
+        //custom drag
+        rb.velocity *= 1 - (Time.fixedDeltaTime * drag);
+    }
+
     private void Update()
     {
         RotateWithMouse();
 
-        LaunchTether();
+        LaunchTether1();
 
         ReelTether();
 
@@ -68,12 +97,9 @@ public class Player : MonoBehaviour
         FireMissile();
 
         Peek();
-
-        if (Input.GetButton("Fire"))
-            rb.velocity = 100 * transform.forward;
     }
 
-    private void RotateWithMouse()
+    private void RotateWithMouse() //run in update
     {
         float yaw = rotateSpeed * Input.GetAxis("Mouse X");
         float pitch = rotateSpeed * Input.GetAxis("Mouse Y");
@@ -82,65 +108,124 @@ public class Player : MonoBehaviour
         transform.Rotate(-pitch, 0, 0, Space.Self);
     }
 
-    private void LaunchTether()
+    private void LaunchTether1() //run in update
     {
+        LaunchTether2(-1, leftCrosshair, "LeftTether", leftLineRenderer, leftAnchor);
+        LaunchTether2(1, rightCrosshair, "RightTether", rightLineRenderer, rightAnchor);
+    }
+
+    private void LaunchTether2(int posOrNeg, TMP_Text crosshair, string tetherInput, LineRenderer tetherRenderer, GameObject anchor)
+    {
+        bool isLeft = posOrNeg == -1;
+        //else isRight, posOrNeg == 1;
+
         //get offset angles
+        //used for raycast:
+        Vector3 hitOffset = raycastOffset * posOrNeg * transform.right;
+        //used only for linerenderers:
+        Vector3 startOffset = (1 * posOrNeg * transform.right) + (-1 * transform.up);
 
-        //used for raycast
-        Vector3 hitOffset = raycastOffset * transform.right;
-        //used only for linerenderers
-        Vector3 startOffset = (transform.right * -1) + (transform.up * -1);
-
-        if (Input.GetButtonDown("LeftTether"))
+        int layerMask = 1 << 7;
+        if (!Physics.Raycast(transform.position, transform.forward + hitOffset, out RaycastHit hit, maxTetherRange, layerMask))
+            crosshair.color = Color.black;
+        else //if terrain is in range
         {
-            int layerMask = 1 << 7;
-            if (Physics.Raycast(transform.position, transform.forward - hitOffset, out RaycastHit hit, 3000, layerMask))
+            crosshair.color = Input.GetButton(tetherInput) ? Color.black : Color.red;
+
+            if (Input.GetButtonDown(tetherInput))
             {
-                leftLineRenderer.enabled = true;
-                leftLineRenderer.SetPosition(1, hit.point);
+                anchor.transform.position = hit.point;
+                if (isLeft)
+                    leftJoint = CreateJoint(leftAnchor);
+                else
+                    rightJoint = CreateJoint(rightAnchor);
+
+                tetherRenderer.enabled = true;
+                tetherRenderer.SetPosition(1, hit.point);
             }
         }
-        else if (Input.GetButtonUp("LeftTether"))
-            leftLineRenderer.enabled = false;
 
-        if (leftLineRenderer.enabled)
-            leftLineRenderer.SetPosition(0, transform.position + startOffset);
-
-        startOffset = (transform.right * 1) + (transform.up * -1);
-
-        if (Input.GetButtonDown("RightTether"))
+        if (Input.GetButtonUp(tetherInput))
         {
-            int layerMask = 1 << 7;
-            if (Physics.Raycast(transform.position, transform.forward + hitOffset, out RaycastHit hit, 3000, layerMask))
+            tetherRenderer.enabled = false;
+            if (isLeft)
+                Destroy(leftJoint);
+            else
+                Destroy(rightJoint);
+        }
+
+        if (tetherRenderer.enabled)
+        {
+            tetherRenderer.SetPosition(0, transform.position + startOffset);
+
+            float distance = Vector3.Distance(transform.position, anchor.transform.position);
+
+            SpringJoint joint = posOrNeg == -1 ? leftJoint : rightJoint;
+            joint.minDistance = 0;
+            //if not reeling
+            if ((isLeft && !leftReeling) || (!isLeft && !rightReeling))
             {
-                rightLineRenderer.enabled = true;
-                rightLineRenderer.SetPosition(0, transform.position + startOffset);
-                rightLineRenderer.SetPosition(1, hit.point);
+                joint.maxDistance = distance;
+                joint.spring = 4.5f;
             }
         }
-        else if (Input.GetButtonUp("RightTether"))
-            rightLineRenderer.enabled = false;
-
-        if (rightLineRenderer.enabled)
-            rightLineRenderer.SetPosition(0, transform.position + startOffset);
     }
 
-    private void ReelTether()
+    private SpringJoint CreateJoint(GameObject jointObject)
+    {
+        SpringJoint joint = jointObject.AddComponent<SpringJoint>();
+        joint.autoConfigureConnectedAnchor = false;
+        joint.connectedAnchor = Vector3.zero;
+        joint.spring = 4.5f;
+        joint.damper = 7;
+        joint.massScale = 4.5f;
+        joint.connectedBody = rb;
+        return joint;
+    }
+
+    private void ReelTether() //run in update
+    {
+        leftReeling = Input.GetButton("LeftReel") && leftJoint != null;
+        rightReeling = Input.GetButton("RightReel") & rightJoint != null;
+
+        float newReelAmount = leftReeling && rightReeling ? doubleReelAmount : reelAmount;
+
+        if (leftReeling)
+        {
+            float distance = Vector3.Distance(transform.position, leftAnchor.transform.position);
+            leftJoint.maxDistance = distance - 10;
+            leftJoint.spring = newReelAmount;
+        }
+        if (rightReeling)
+        {
+            float distance = Vector3.Distance(transform.position, rightAnchor.transform.position);
+            rightJoint.maxDistance = distance - 10;
+            rightJoint.spring = newReelAmount;
+        }
+    }
+
+    private void Gas() //run in update
+    {
+        if (Input.GetButtonDown("Gas"))
+            StartCoroutine(GasDelay());
+
+        if (Input.GetButton("Gas")) //adjust this so that it only works if gas remains!
+            drag = 0;
+        else
+            drag = .5f;
+    }
+    private IEnumerator GasDelay()
+    {
+        yield return new WaitForSeconds(.8f);
+        rb.velocity *= gasBoost;
+    }
+
+    private void FireMissile() //run in update
     {
 
     }
 
-    private void Gas()
-    {
-
-    }
-
-    private void FireMissile()
-    {
-
-    }
-
-    private void Peek()
+    private void Peek() //run in update
     {
         if (Input.GetButtonDown("Peek") && rb.velocity != Vector3.zero)
             transform.rotation = Quaternion.LookRotation(rb.velocity.normalized, Vector3.up);
